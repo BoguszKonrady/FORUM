bf03189728da167e9cfcb1c1cba8fd7a4a1b450b
Dominik <62477432+imdominiq@users.noreply.github.com>
2024-05-31 18:27:44 +0200

    Icons and colors
    Add colors and icons for buttons

--- app/index.php
+++ app/index.php
@@ -38,7 +38,8 @@ function timeAgo($datetime, $full = false) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Prosta Strona</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <link href="style.css" rel="stylesheet"> 
+    <link href="style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
 
@@ -50,27 +51,27 @@ function timeAgo($datetime, $full = false) {
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-                <a class="nav-link" href="index.php">Strona główna</a>
+                <a class="nav-link" href="index.php"><i class="fa-solid fa-house"></i></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Ulubione</a>
+                <a class="nav-link" href="#">Ulubione <i class="fa-solid fa-heart"></i></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Mój profil</a>
+                <a class="nav-link" href="#">Mój profil <i class="fa-solid fa-user"></i></a>
             </li>
         </ul>
         <form class="form-inline mx-auto search-form">
-            <input class="form-control mr-sm-2 search-input" type="search" placeholder="Szukaj" aria-label="Szukaj">
+            <input class="form-control mr-sm-2 search-input" type="search" placeholder="Szukaj..." aria-label="Szukaj">
         </form>
         <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-                <a class="nav-link" href="logout.php">Wiadomości</a>
+                <a class="nav-link" href="logout.php">Wiadomości <i class="fa-solid fa-message"></i></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="logout.php">Powiadomienia</a>
+                <a class="nav-link" href="logout.php">Powiadomienia <i class="fa-solid fa-bell"></i></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="logout.php">Wyloguj się</a>
+                <a class="nav-link" href="logout.php">Wyloguj się <i class="fa-solid fa-right-from-bracket"></i></a>
             </li>
         </ul>
     </div>
@@ -80,10 +81,10 @@ function timeAgo($datetime, $full = false) {
     <div class="row">
         <div class="col-md-2">
             <div class="sidebar">
-                <button class="btn btn-outline-primary btn-block">Znajomi</button>
-                <button class="btn btn-outline-primary btn-block">Wydarzenia</button>
-                <button class="btn btn-outline-primary btn-block">Grupy</button>
-                <button class="btn btn-outline-primary btn-block">Ulubione</button>
+                <button class="btn btn-outline-primary btn-block">Znajomi <i class="fa-solid fa-user-group"></i></button>
+                <button class="btn btn-outline-primary btn-block">Wydarzenia <i class="fa-solid fa-calendar-days"></i></button>
+                <button class="btn btn-outline-primary btn-block">Grupy <i class="fa-solid fa-people-group"></i></button>
+                <button class="btn btn-outline-primary btn-block">Ulubione <i class="fa-solid fa-heart"></i></button>
             </div>
         </div>
         <div class="col-md-8">
@@ -95,10 +96,10 @@ function timeAgo($datetime, $full = false) {
                                 <textarea class="form-control" name="content" rows="3" placeholder="Co masz na myśli?"></textarea>
                             </div>
                             <div class="form-group">
-                                <button type="button" class="btn btn-outline-primary btn-sm" onclick="document.getElementById('uploadImage').click()">Dodaj zdjęcie</button>
+                                <button type="button" class="btn btn-outline-primary btn-sm" onclick="document.getElementById('uploadImage').click()">Dodaj zdjęcie <i class="fa-solid fa-image"></i></button>
                                 <input type="file" name="image" id="uploadImage" style="display: none;">
-                                <button type="button" class="btn btn-outline-secondary btn-sm">Dodaj lokalizację</button>
-                                <button type="submit" class="btn btn-primary btn-sm">Opublikuj</button>
+                                <button type="button" class="btn btn-outline-secondary btn-sm">Dodaj lokalizację <i class="fa-solid fa-location-dot"></i></button>
+                                <button type="submit" class="btn btn-primary btn-sm btn-submit">Opublikuj <i class="fa-solid fa-paper-plane"></i></button>
                             </div>
                         </form>
                     </div>
@@ -123,11 +124,11 @@ function timeAgo($datetime, $full = false) {
                                     <h5 class='card-title'>{$row['title']}</h5>
                                     <p class='card-text post-description'>{$row['content']}</p>
                                     <div class='post-reactions'>
-                                        <button type='button' class='btn btn-outline-primary btn-sm'>Like</button>
-                                        <button type='button' class='btn btn-outline-danger btn-sm'>Serce</button>
-                                        <button type='button' class='btn btn-outline-warning btn-sm'>Wow</button>
-                                        <button type='button' class='btn btn-outline-secondary btn-sm btn-share'>Udostępnij</button>
-                                        <button type='button' class='btn btn-outline-info btn-sm btn-favorite'>Dodaj do ulubionych</button>
+                                        <button type='button' class='btn btn-outline-primary btn-sm'><i class='fa-solid fa-thumbs-up'></i></button>
+                                        <button type='button' class='btn btn-outline-danger btn-sm'><i class='fa-solid fa-heart'></i></button>
+                                        <button type='button' class='btn btn-outline-warning btn-sm'><i class='fa-solid fa-face-surprise'></i></button>
+                                        <button type='button' class='btn btn-outline-secondary btn-sm btn-share'><i class='fa-solid fa-share'></i></button>
+                                        <button type='button' class='btn btn-outline-info btn-sm btn-favorite'>Dodaj do <i class='fa-solid fa-heart'></i></button>
                                     </div>
                                     <div class='comment-section'>
                                         <input type='text' class='form-control' placeholder='Dodaj komentarz...'>
