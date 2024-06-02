938c4f5b16a37c887ddaadd111b2ac697004c16e
Maksymilian <maksiur1234@gmail.com>
2024-06-02 21:39:35 +0200

    register system

---
+++ app/views/layout/loggedUserNavbar.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-lg navbar-light menu">
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php"><i class="fa-solid fa-house"></i></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Ulubione <i class="fa-solid fa-heart"></i></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Mój profil <i class="fa-solid fa-user"></i></a>
+            </li>
+        </ul>
+        <form class="form-inline mx-auto search-form">
+            <input class="form-control mr-sm-2 search-input" type="search" placeholder="Szukaj..." aria-label="Szukaj">
+        </form>
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="/views/posts/userPosts.php">Twoje posty <i class="fa-solid fa-circle-user"></i></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Wiadomości <i class="fa-solid fa-message"></i></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Powiadomienia <i class="fa-solid fa-bell"></i></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/controllers/LogoutController.php">Wyloguj się <i class="fa-solid fa-right-from-bracket"></i></a>
+            </li>
+        </ul>
+    </div>
+</nav>
